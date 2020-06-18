942b486a989f2f795cc0fc0a786377141c9bbd3d
extrahappyperson <ht.uzpharma@gmail.com>
2020-06-18 11:06:32 +0200

    container

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -76,23 +76,15 @@
             </div>
 
             <div class="links">
-                <a href="https://laracasts.com">Docs</a>
-
-                @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                    <a href="{{ url('/home') }}">Home</a>
-                    @else
-                    <a href="{{ route('login') }}">Login</a>
-
-                    @if (Route::has('register'))
-                    <a href="{{ route('register') }}">Register</a>
-                    @endif
-                    @endauth
-                </div>
-                @endif
+                @auth
+                <a href="{{ url('/home') }}">Home</a>
+                @else
+                <a href="{{ route('login') }}">Login</a>
+                <a href="{{ route('register') }}">Register</a>
+                @endauth
             </div>
         </div>
+    </div>
     </div>
 </body>
 
