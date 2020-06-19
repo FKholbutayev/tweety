f4281d05d52494f8f9c251c5f246e402085778bf
extrahappyperson <ht.uzpharma@gmail.com>
2020-06-19 13:56:52 +0200

    user follows migration

--- app/User.php
+++ app/User.php
@@ -44,4 +44,8 @@ class User extends Authenticatable {
     public function getAvatarAttribute() {
         return "https://i.pravatar.cc/40?u=" .$this->email;
     }
+
+    public function follows() {
+        return $this->belongsToMany(User::class, 'follows', 'user_id', 'following_user_id');
+    }
 }
--- resources/views/_friends-list.blade.php
+++ resources/views/_friends-list.blade.php
@@ -1,11 +1,11 @@
-<h3 class="font-bold text-xl mb-4">Friends</h3>
+<h3 class="font-bold text-xl mb-4">Following</h3>
 <ul>
-    @foreach (range(1, 8) as $item)
+    @foreach (auth()->user()->follows as $user)
 
     <li class="mb-4">
         <div class="flex items-center text-sm">
-            <img src="https://i.pravatar.cc/40" class="rounded-full mr-2" alt="">
-            Joe Doe
+            <img src="{{$user->avatar}}" alt="User avatar">
+            {{$user->name}}
         </div>
     </li>
     @endforeach
