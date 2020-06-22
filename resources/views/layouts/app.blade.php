68c97f2acaaf306165382bd2df0d8713fa06a40e
extrahappyperson <ht.uzpharma@gmail.com>
2020-06-22 17:50:56 +0200

    display sidebar for auth users

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,17 +32,22 @@
         </section>
         <section class="px-8">
             <main class="container mx-auto">
-                <div class="lg:flex lg:justify-between">
+                <div class="lg:flex lg:justify-center">
+                    @if(auth()->check())
                     <div class="lg:w-32">
                         @include('_sidebar-links')
                     </div>
-                    <div class="lg:flex-1 lg:mx-10" style="max-width: 700px">
+                    @endif
 
+                    <div class="lg:flex-1 lg:mx-10" style="max-width: 700px">
                         @yield('content')
                     </div>
+
+                    @if (auth()->check())
                     <div class="lg:w-1/6 bg-blue-100 rounded-lg p-4">
                         @include('_friends-list')
                     </div>
+                    @endif
                 </div>
             </main>
         </section>
--- routes/web.php
+++ routes/web.php
@@ -21,8 +21,8 @@ Route::middleware('auth')->group(function() {
     Route::get('/tweets', 'TweetController@index')->name('home');
     Route::post('/tweets', 'TweetController@store');
 });
-
 Route::get('/profiles/{user}', 'ProfilesController@show')->name('profile');
+
 
 Auth::routes();
 
