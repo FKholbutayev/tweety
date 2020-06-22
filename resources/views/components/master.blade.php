a7ae2376708ccfc64b1ff1fd9aa2f45e04518bc1
extrahappyperson <ht.uzpharma@gmail.com>
2020-06-22 18:08:51 +0200

    components x-master added

--- resources/views/components/master.blade.php
+++ resources/views/components/master.blade.php
@@ -30,27 +30,7 @@
                 </h1>
             </header>
         </section>
-        <section class="px-8">
-            <main class="container mx-auto">
-                <div class="lg:flex lg:justify-center">
-                    @if(auth()->check())
-                    <div class="lg:w-32">
-                        @include('_sidebar-links')
-                    </div>
-                    @endif
-
-                    <div class="lg:flex-1 lg:mx-10" style="max-width: 700px">
-                        @yield('content')
-                    </div>
-
-                    @if (auth()->check())
-                    <div class="lg:w-1/6 bg-blue-100 rounded-lg p-4">
-                        @include('_friends-list')
-                    </div>
-                    @endif
-                </div>
-            </main>
-        </section>
+        {{ $slot }}
     </div>
 </body>
 
