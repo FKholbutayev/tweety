c100a799707234b41f42e91131cd6e78a7f05b08
extrahappyperson <ht.uzpharma@gmail.com>
2020-07-01 17:43:18 +0200

    like syste logic

--- app/Tweet.php
+++ app/Tweet.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use App\User;
+use App\Like;
 
 class Tweet extends Model {
     
@@ -11,5 +12,37 @@ class Tweet extends Model {
 
     public function user() {
         return $this->belongsTo(User::class);
+    }
+
+    public function likes() {
+        return $this->hasMany(Like::class);
+    }
+
+    public function like($user = null, $liked = true) {
+        
+        $this->likes()->updateOrCreate([
+            'user_id' => $user ? $user->id : auth()->user(),
+
+        ], [
+            'liked' => $liked
+        ]);
+    }
+
+    public function isLikedBy(User $user) {
+        return (bool) $user->likes
+                ->where('tweet_id', $this->id)
+                ->where('liked', true)
+                ->count();
+    }
+
+    public function isDislikedBy(User $user) {
+        return (bool) $user->likes
+                ->where('tweet_id', $this->id)
+                ->where('liked', false)
+                ->count();
+    }
+
+    public function dislike($user = null) {
+        $this->like($user, false);
     }
 }
